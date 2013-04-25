a3f6a0138b36545060a9ef55d3e3dbd0b546d332
Andres Echeverri <andresecheverr@misena.edu.co>
2013-04-24 21:58:04 -0500

    davilitado la imagen obligatoria en estudiantes

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -32,7 +32,8 @@ end
                   :url  => "/assets/estudiantes/:id/:style/:basename.:extension",
                   :path => ":rails_root/public/assets/estudiantes/:id/:style/:basename.:extension"
 
-validates_attachment_presence :imagen
+
+#validates_attachment_presence :imagen
 validates_attachment_size :imagen, :less_than => 5.megabytes
 validates_attachment_content_type :imagen, :content_type => ['image/jpeg', 'image/png']
 end
